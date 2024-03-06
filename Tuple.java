4c989ba36d1fc45cfb71e5484cc8d65bb97f6a31
igor <129679728+yaigoryao@users.noreply.github.com>
2024-03-06 21:40:40 +0300

    Add files via upload

---
+++ Tuple.java
@@ -0,0 +1,16 @@
+package csv;
+public class Tuple<T1, T2>
+{
+    public T1 first;
+    public T2 second;
+    public Tuple(T1 first, T2 second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+    public Tuple()
+    {
+        this.first = null;
+        this.second = null;
+    }
+}
